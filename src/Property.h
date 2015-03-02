5eabe03e7b9c9f0a598b787e8b87803cbeb8c58e
Gazben <bencegazder@gmail.com>
2015-03-02 11:45:38 +0100

    Add to tree and delete whole tree added. Basic test added.

--- src/Property.h
+++ src/Property.h
@@ -1,44 +1,94 @@
 /*
-The property is made of contraints. 
+The property is made of contraints.
 */
 
 /*
 TODO:
-   - build up a binary tree
-   - 
+- build up a binary tree
+-
 
 */
 #include <limits.h>
+#include <stdlib.h>
 
 #define EVENT_A 0x1
 #define EVENT_B 0x2
 #define EVENT_C 0x4
 #define EVENT_D 0x8
 
+int stateRegister;
 
- int stateRegister;
+/* TEST FUNCTIONS */
+void addEvent(int event_code){
+  stateRegister = stateRegister | event_code;
+}
+
+void removeEvent(int event_code){
+  stateRegister = stateRegister & (INT_MAX ^ event_code);
+}
 
 typedef enum OutputState{
   TRUE, FALSE, UNKNOWN
-};
+}OutputState;
+
+typedef OutputState(*evalFunctionType)(int stateRegisterCopy);
+
+/* TEST EVAL FUNCTIONS */
+OutputState evalImpl_U(int stateRegisterCopy){
+  return UNKNOWN;
+}
+OutputState evalImpl_T(int stateRegisterCopy){
+  return TRUE;
+}
+OutputState evalImpl_F(int stateRegisterCopy){
+  return FALSE;
+}
 
 typedef struct Property{
-
   struct Property* rootNode;
   struct Property* leftNode;
   struct Property* rightNode;
 
   int stateRegisterCopy;
+  evalFunctionType evalFunction;
+}Property;
 
-};
+Property* createEmptyProperty(){
+  Property* newProperty = (Property*)malloc(sizeof(Property));
+  newProperty->leftNode = NULL;
+  newProperty->rightNode = NULL;
+  newProperty->rootNode = NULL;
+  newProperty->evalFunction = NULL;
 
-/* TEST FUNCTIONS */
-void addEvent( int event_code ){
-  stateRegister = stateRegister | event_code;
+  return newProperty;
 }
 
-void removeEvent( int event_code){
-  stateRegister = stateRegister & (INT_MAX ^ event_code);
+Property* addNewPropertyToRoot(int stateRegisterCopy, Property* rootproperty, evalFunctionType evalFunction){
+  Property* tempPropertyPtr = createEmptyProperty();
+  tempPropertyPtr->rootNode = rootproperty;
+
+  tempPropertyPtr->stateRegisterCopy = stateRegisterCopy;
+  tempPropertyPtr->evalFunction = evalFunction;
+
+  if (rootproperty == NULL){
+    rootproperty = tempPropertyPtr;
+    tempPropertyPtr->rootNode = NULL;
+  }
+  else if (rootproperty->leftNode == NULL){
+    rootproperty->leftNode = tempPropertyPtr;
+  }
+  else{
+    rootproperty->rightNode = tempPropertyPtr;
+  }
+
+  return rootproperty;
 }
 
+void freePropertyTree(Property* root) {
+  if (root == NULL)
+    return;
 
+  freePropertyTree(root->rightNode);
+  freePropertyTree(root->leftNode);
+  free(root);
+}
--- src/main.c
+++ src/main.c
@@ -2,16 +2,23 @@
 
 #include "Property.h"
 
-
 int main(){
+  stateRegister = 0;
 
   addEvent(EVENT_A);
-  printf( "%d\n", stateRegister );  //1
+  printf("%d\n", stateRegister);  //1
   addEvent(EVENT_B);
   printf("%d\n", stateRegister);  //3
   removeEvent(EVENT_A);
   printf("%d\n", stateRegister);  //2
 
+  /*TEST1*/
+  Property* root = NULL;
+  root = addNewPropertyToRoot(stateRegister, root, evalImpl_U);
+  addNewPropertyToRoot(stateRegister, root, evalImpl_T);
+  addNewPropertyToRoot(stateRegister, root, evalImpl_F);
+
+  freePropertyTree(root);
 
   getchar();
   return 0;
