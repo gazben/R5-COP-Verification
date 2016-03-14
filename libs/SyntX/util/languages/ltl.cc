6233e0a7b20f77d51c4ab5bf53a60653a6fcccaa
Gazder Bence <bencegazder@gmail.com>
2016-03-14 19:04:16 +0100

    LTL parser now accepts strings as variables

--- libs/SyntX/util/languages/ltl.cc
+++ libs/SyntX/util/languages/ltl.cc
@@ -28,7 +28,7 @@ bool ltl::match(base_rule::match_range& context, base_rule::match_range& result,
   rule RPAR;
   RPAR <<= character(")");
   rule AtomicProposition;
-  AtomicProposition <<= range('0', '9');
+  AtomicProposition <<= +(range('a', 'z')) << *(range('a', 'z') | range('A', 'Z') | (range('0', '9')));
 
   rule OperatorTwoOp;
   OperatorTwoOp <<=
