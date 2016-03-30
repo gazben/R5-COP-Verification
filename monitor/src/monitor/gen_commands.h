96c3bdd24f901cbe7edc088891e8b09814832365
Gazder Bence <bencegazder@gmail.com>
2016-03-30 12:12:03 +0200

    GEN: test commands added into the DEBUG_NO_ROS build

--- monitor/src/monitor/gen_commands.h
+++ monitor/src/monitor/gen_commands.h
@@ -4,8 +4,14 @@
 #include <string>
 
 //True command
+#ifndef DEBUG_NO_ROS
 std::string true_command = "--true_command--";
-//False command
 std::string false_command = "--false_command--";
+#else
+std::string true_command = "echo result TRUE";
+std::string false_command = "echo result FALSE";
+#endif
+//False command
+
 
 #endif // gen_commands_h__
